50fea93efac17d850e454323cb4e74def577b953
alex-skxy <aleksandra.kina@sluz.ch>
2022-05-18 14:30:55 +0200

    backend stuff

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from 'express';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { router as movieRouter } from './movie/index.js';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {orderRouter} from "./server/orderRouter.js";
+import {checkoutRouter} from "./server/checkoutRouter.js";
 
 const app = express();
 
 app.use(express.static(`${dirname(fileURLToPath(import.meta.url))}/public`));
 
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
-app.use('/api', orderRouter);
-
-app.get('/', (request, response) => response.redirect('/movie'));
+app.use('/api/orders/', orderRouter);
+app.use('/api/checkout/', checkoutRouter);
 
 app.listen(8080, () => {
     console.log('Server is listening to http://localhost:8080');
---
+++ server/model.js
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+/*
+fs.readFile('db.json', 'utf8', (err, data) => {
+
+});
+*/
+
+// TODO implement model
+
+const getOrders = async () => {
+    // TODO implement
+};
+const getOrder = async number => {
+    // TODO implement
+};
+const createOrder = async order => {
+    // TODO implement
+};
+const updateOrder = async order => {
+    // TODO implement
+};
+const archiveOrder = async number => {
+// TODO implement
+}
